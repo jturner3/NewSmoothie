c117a63f79a9119fa255a513e31f2d1bd1aee2a8
John <317horus@gmail.com>
2016-10-17 15:27:45 -0400

    first commit

---
+++ src/org/elevenfifty/smoothie/fruit/Pineapple.java
@@ -0,0 +1,22 @@
+package org.elevenfifty.smoothie.fruit;
+
+public class Pineapple extends Fruit implements Coreable {
+	boolean cored;
+	
+	public Pineapple(){
+		super("Pineapple");
+	}
+	@Override
+	public void core() {
+		if (!cored) {
+			cored = true;
+			System.out.println("Cored an " + getName());
+		}
+	}
+
+	@Override
+	public boolean isCored() {
+		return cored;
+		
+	}
+}
